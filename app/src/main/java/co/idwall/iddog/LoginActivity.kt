d54fe1fe75ee231fa0f19ea4eacc512e8a3acd32
Vaniel Silva <vaniel@sigmast.com.br>
2019-04-18 15:49:12 -0300

    Melhorando inicialização de login

--- app/src/main/java/co/idwall/iddog/LoginActivity.kt
+++ app/src/main/java/co/idwall/iddog/LoginActivity.kt
@@ -21,8 +21,6 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
-        button_sign_in.setOnClickListener { attemptLogin() }
     }
 
     override fun onResume() {
@@ -30,7 +28,7 @@ class LoginActivity : AppCompatActivity() {
         if (Util.getToken(this) != null) {
             finish() // Don't show login
         } else {
-        super.onResume()
+            super.onResume()
         }
     }
 
